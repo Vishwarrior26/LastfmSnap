009d7f1f6a1b5602443ea0b4600ca4d87fbe3659
Vishwarrior26 <srimathkandali@gmail.com>
2021-11-14 15:29:29 -0800

    Changed algorithm

--- scrape.py
+++ scrape.py
@@ -4,17 +4,19 @@ import requests
 
 class scrape:
 
-    # def __init__(self, start, end="start"):
-    #     if end == "start":
-    #         end = start
-    #     self.url = "https://www.last.fm/user/vishwarrior/library/artists?from=" + \
-    #         str(start) + "&to=" + str(end)
     def __init__(self, type, start, end="start"):
         if end == "start":
             end = start
-        self.url = "https://www.last.fm/user/vishwarrior/library/"+ type +"?from=" + \
+        self.start = start
+        self.end = end
+        self.url = "https://www.last.fm/user/vishwarrior/library/" + type + "?from=" + \
             str(start) + "&to=" + str(end)
-    def getInfo(self):
+
+    def __setType(self, type):
+        self.url = "https://www.last.fm/user/vishwarrior/library/" + type + "?from=" + \
+            str(self.start) + "&to=" + str(self.end)
+
+    def __info(self):
         req = requests.get(self.url)
         soup = BeautifulSoup(req.text, "html.parser")
         description = soup.find_all("meta", property="og:description")
@@ -28,10 +30,24 @@ class scrape:
         test = []
         for temper in templist:
             play = temper.split(" ")[-1]
-            artist = temper[:temper.find(play) - 1]
-            test.append([artist, play])
+            type = temper[:temper.find(play) - 1]
+            test.append([type, play])
         return test
 
+    def artistInfo(self):
+        self.__setType("artists")
+        return self.__info()
 
-sc = scrape("artists","2021-09-01", "2021-09-30")
-print(sc.getInfo())
+    def albumInfo(self):
+        self.__setType("albums")
+        return self.__info()
+
+    def trackInfo(self):
+        self.__setType("tracks")
+        return self.__info()
+
+
+sc = scrape("tracks", "2021-09-01", "2021-09-30")
+print(sc.artistInfo())
+print(sc.albumInfo())
+print(sc.trackInfo())
