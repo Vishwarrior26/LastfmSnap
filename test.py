4806fe60a2f3c45968c608a8a144ca3114a090e9
Vishwarrior26 <srimathkandali@gmail.com>
2021-11-14 14:03:04 -0800

    Original dev of scrape alg, now deprecated

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 url = "https://www.last.fm/user/vishwarrior/library/artists?from=2021-11-10&to=2021-11-10"
+# url = "https://www.last.fm/user/vishwarrior/library/artists?from=2020-08-01&to=2021-11-14"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 #print(soup)
@@ -12,13 +13,28 @@ fini = temp.find('"',init+1)
 # print(fini)
 # print(temp[init:fini])
 # print(temp[init:fini].split(")"))
-print(temp[init:fini].replace("(","").split(")"))
+# print(temp[init:fini].replace("(","").split(")"))
 templist = temp[init:fini].replace("(","").split(")")
 for i in range(1,len(templist)):
     templist[i] = templist[i][2:]
-print(templist)
+# print(templist)
+del templist[-1] # check again later if necessary
+# print(templist)
+artists = []
+plays = []
 for temper in templist:
-    print(temper)
+    # print(temper)
+    play = temper.split(" ")[-1]
+    # print(temper.find(play))
+    artist = temper[:temper.find(play)-1]
+    # print(len(artist))
+    # print(artist)
+    # print(temper.split(" "))
+    # print(temper.split(" ")[-1])
+    plays.append(play)
+    artists.append(artist)
+print(artists)
+print(plays)
 # print(temp)
 # print(description[1])
 # descriptionArray = []
