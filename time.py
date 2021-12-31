f55618a12a85f4a99e17705e078657e05977f6b0
Vishwarrior26 <srimathkandali@gmail.com>
2021-12-31 13:31:48 -0800

    Some testing and stuff

--- time.py
+++ time.py
@@ -1,12 +1,20 @@
 from mutagen.mp3 import MP3
 import scrape
 
-sc = scrape.scrape(1, "2021-08-15")
-tempinfo = sc.trackInfo()[0]
+# sc = scrape.scrape(10, "2021-08-15", "TODAY")
+sc = scrape.scrape("MAX", "2020-08-15", "TODAY")
+print(sc.artistInfo())
+# tempinfo = sc.trackInfo()
 # print(tempinfo)
-path = "D:\\Music\\" + tempinfo[0] + "\\" + tempinfo[1] + ".mp3"
-# print(path)
-audio = MP3(path)
-print(audio.info.length)
-# print(round(int(audio.info.length) * int(tempinfo[2]) / 60, 3))
-print(round(int(audio.info.length) * tempinfo[2] / 60, 3))
+# for temp in tempinfo:
+#     artist = temp[0]
+#     track = temp[1]
+#     plays = temp[2]
+#     artist = artist.replace("&quot;", " ").lstrip(' ')
+#     track = track.replace(":", " ")
+#     path = "D:\\Music\\" + artist + "\\" + track + ".mp3"
+#     # print(path)
+#     audio = MP3(path)
+#     # print(round(int(audio.info.length) * plays / 60, 3))
+#     temp.append(round(int(audio.info.length) * plays / 60, 3))
+# print(tempinfo)
