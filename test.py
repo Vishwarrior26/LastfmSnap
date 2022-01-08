e195bbba795f13854e59f53186f4f747f6b25088
Vishwarrior26 <srimathkandali@gmail.com>
2022-01-08 10:57:33 -0800

    Cleaner implementation of artistcount

--- test.py
+++ test.py
@@ -9,12 +9,10 @@ tempinfo = sc.trackInfo()
 for temp in tempinfo:
     artist = temp[0]
     if artist not in (row[0] for row in artists):
-        artists.append([artist, 0])
-
-for temper in artists:
-    artist = temper[0]
-    for temp in tempinfo:
-        if temp[0] == artist:
-            temper[-1] += 1
+        artists.append([artist, 1])
+    else:
+        for row in artists:
+            if row[0] == artist:
+                row[-1] += 1
 
 print(sorted(artists, key=itemgetter(-1), reverse=True))
