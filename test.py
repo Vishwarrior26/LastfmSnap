86cb927f39514a0a32cb7aa64486e1896d987310
Vishwarrior26 <srimathkandali@gmail.com>
2022-01-11 20:07:03 -0800

    Testing

--- test.py
+++ test.py
@@ -1,20 +1,5 @@
-from operator import itemgetter
 import scrape
 import csv
 
-sc = scrape.scrape()
-# artists = []
-# tempinfo = sc.trackInfo()
-#
-# for temp in tempinfo:
-#     artist = temp[0]
-#     if artist not in (row[0] for row in artists):
-#         artists.append([artist, 1])
-#     else:
-#         for row in artists:
-#             if row[0] == artist:
-#                 row[-1] += 1
-#
-# print(sorted(artists, key=itemgetter(-1), reverse=True))
-
+sc = scrape.scrape("100", "ALL")
 print(sc.artistCounts())
