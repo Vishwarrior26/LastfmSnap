3a5880623646a99f395dff415d58fc031892c0b2
Visruth <vsrimath@calpoly.edu>
2024-02-04 14:31:32 -0800

    Trying to speedup still

--- LastCSV.jl
+++ LastCSV.jl
@@ -1,15 +1,15 @@
-using DataFrames, Dates, HTTP, LightXML, CSV
+using DataFrames, Dates, HTTP, LightXML, CSV, Distributed
 
-const baseUrl::String = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=xml&limit=1000&api_key=959357ab2524c0d50de6e4ee8e792c68&user="
 
 """
-    getXML(username, page)
+getXML(username, page)
 
 Get XML response from Lastfm API for given username.
     
-Gets 1000 tracks per page for processing. Username should be well formatted, without leading or trailing whitespaces or anything of the kind.
-"""
+    Gets 1000 tracks per page for processing. Username should be well formatted, without leading or trailing whitespaces or anything of the kind.
+        """
 function getXML(username, page)
+    baseUrl::String = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=xml&limit=1000&api_key=959357ab2524c0d50de6e4ee8e792c68&user="
     try
         return parse_string(String(HTTP.get(baseUrl * username * "&page=" * string(page)).body))
     catch e
@@ -24,28 +24,28 @@ end
 Get the tracks associated with the given XML document and put them into `tracks`, modifying it in place.
 
 """
-function parseXMLPage!(tracks, xdoc)
-    pageTracks = collect(child_elements(collect(child_elements(root(xdoc)))[1]))
-    if has_attribute(pageTracks[1], "nowplaying")
-        popfirst!(pageTracks)
+function parseXMLPage!(scrobbles, xdoc)
+    pageScrobbles = collect(child_elements(collect(child_elements(root(xdoc)))[1]))
+    if has_attribute(pageScrobbles[1], "nowplaying")
+        popfirst!(pageScrobbles)
     end
-    append!(tracks, pageTracks)
+    append!(scrobbles, pageScrobbles)
 end
 
 """
     parseDate(track)
 
-Parse a date as given by the Last.fm API into a DateTime object
+Parse a date as given by the Last.fm API into a DateTime object. Times are in UTC.
 """
-function parseDate(track)
-    return parse(DateTime, content(find_element(track, "date")), dateformat"dd u yyyy, HH:MM")
+function parseDate(scrobble)
+    return parse(DateTime, content(find_element(scrobble, "date")), dateformat"dd u yyyy, HH:MM")
 end
 
-function parseTrackXMLElement(track)
-    dateTime = parseDate(track)
-    trackTitle = content(find_element(track, "name"))
-    album = content(find_element(track, "album"))
-    artist = content(find_element(track, "artist"))
+function parseTrackXMLElement(scrobble)
+    dateTime = parseDate(scrobble)
+    trackTitle = content(find_element(scrobble, "name"))
+    album = content(find_element(scrobble, "album"))
+    artist = content(find_element(scrobble, "artist"))
     return (dateTime, trackTitle, album, artist)
 end
 
@@ -57,20 +57,44 @@ Get all available last.fm data for this user.
 function getLastfmData(username)
     xdoc = getXML(username, 1)
     totalPages = parse(Int64, attributes_dict(root(xdoc)["recenttracks"][1])["totalPages"])
-    totalTracks = parse(Int64, attributes_dict(root(xdoc)["recenttracks"][1])["total"])
-    tracks = Vector{XMLElement}()
-    sizehint!(tracks, totalTracks)
-    parseXMLPage!(tracks, xdoc)
+    totalScrobbles = parse(Int64, attributes_dict(root(xdoc)["recenttracks"][1])["total"])
+    scrobbles = Vector{XMLElement}()
+    sizehint!(scrobbles, totalScrobbles)
+    parseXMLPage!(scrobbles, xdoc)
 
-    # Asynchronous multithreading to greatly speed up processing time 
     @sync Threads.@threads for page = 2:totalPages
-        @async parseXMLPage!(tracks, getXML(username, page))
+        @async parseXMLPage!(scrobbles, getXML(username, page))
     end
 
-    df = DataFrame(parseTrackXMLElement.(tracks))
+    dates = Vector{DateTime}(undef, totalScrobbles)
+    tracks = Vector{String}(undef, totalScrobbles)
+    albums = Vector{String}(undef, totalScrobbles)
+    artists = Vector{String}(undef, totalScrobbles)
 
+    Threads.@threads for i in 1:totalScrobbles
+        scrobble = scrobbles[i]
+        dates[i] = parseDate(scrobble)
+        tracks[i] = content(find_element(scrobble, "name"))
+        albums[i] = content(find_element(scrobble, "album"))
+        artists[i] = content(find_element(scrobble, "artist"))
+    end
+
+
+    df = DataFrame(
+        date=dates,
+        track=tracks,
+        album=albums,
+        artist=artists
+    )
+
+    # df = DataFrame(parseTrackXMLElement.(scrobbles))
     return sort!(df)
 end
 
+using BenchmarkTools
+@profview @btime getLastfmData("vishwarrior")
+@profview @btime getLastfmData("vishwarrior")
+
 username = ARGS[1]
 CSV.write(username * ".csv", getLastfmData(username), header=["date", "track", "album", "artist"])
+println("Wrote to " * username * ".csv")
